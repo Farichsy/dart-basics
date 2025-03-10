a6a5cda8d14ab473be1ad308ea15dae6edaf8028
Farichsy <farichemosi01@gmail.com>
2025-03-11 01:27:01 +0700

    string

---
+++ #4 double.dart
@@ -0,0 +1,9 @@
+#4
+void main() {
+    double p = 1000; // Pinjaman
+    double t = 2; // Waktu per tahun
+    double r = 5; // Bunga pertahun
+
+    double bunga = (p*t*r) / 1000;
+    print ("bunga sederhana : $bunga");
+}
